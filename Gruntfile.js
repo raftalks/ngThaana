431e298722f2e48adf4bdfba8cee119bc71e9291
Raf <7909000@gmail.com>
2014-01-31 01:06:41 +0500

    ADD build task for Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,96 @@
 module.exports = function(grunt) {
-	
-	grunt.initConfig({
-
-		karma: {
-			unit: {
-				configFile: 'karma.conf.js',
-				background: true,
-				browsers: ['PhantomJS']
-			},
-
-			continious: {
-				configFile: 'karma.conf.js',
-				singleRun: true,
-				browsers: ['PhantomJS']
-			}
-		},
-
-		watch: {
-			karma: {
-				files: [
-					'bower_components/angular/angular.js',
-					'bower_components/angular-mocks/angular-mocks.js',
-					'src/thaana.angular.js',
-					'test/unit/**/*.spec.js'
-				],
-				tasks: ['karma:unit:run']
-			}
-		}
-	});
+    
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
 
 
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-karma');
+    grunt.initConfig({
 
-	grunt.registerTask('testmode', ['karma:unit', 'watch:karma']);
+         meta: {
+          banner: 
+            '/**\n' +
+            ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            ' * <%= pkg.homepage %>\n' +
+            ' *\n' +
+            ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' */\n'
+        },
 
-	grunt.registerTask('testci', ['karma:continious', 'watch']);
+        pkg: grunt.file.readJSON("package.json"),
+
+       
+
+        clean : ['dist/**/*'],
+
+        concat : {
+            options: {
+                  banner: '<%= meta.banner %>',
+                  nonull: true,
+                },
+            dist: {
+                
+                src: ['src/thaana.angular.js'],
+                dest: 'dist/thaana.angular.js'
+                
+            }
+        },
+
+
+        uglify : {
+            dist : {
+                options: {
+                  banner: '<%= meta.banner %>'
+                },
+
+                files : {
+                    'dist/thaana.angular.min.js' : 'src/thaana.angular.js'
+                }
+            }
+        },
+
+
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                background: true,
+                browsers: ['PhantomJS']
+            },
+
+            continious: {
+                configFile: 'karma.conf.js',
+                singleRun: true,
+                browsers: ['PhantomJS']
+            }
+        },
+
+        watch: {
+            karma: {
+                files: [
+                    'bower_components/angular/angular.js',
+                    'bower_components/angular-mocks/angular-mocks.js',
+                    'src/thaana.angular.js',
+                    'test/unit/**/*.spec.js'
+                ],
+                tasks: ['karma:unit:run']
+            }
+        },
+
+
+    });
+
+
+
+
+    grunt.registerTask('testmode', ['karma:unit', 'watch:karma']);
+
+    grunt.registerTask('testci', ['karma:continious', 'watch']);
+
+    grunt.registerTask('build', ['karma:unit', 'clean', 'concat:dist', 'uglify:dist']);
 
 };
