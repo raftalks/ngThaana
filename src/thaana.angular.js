b9ad684c3ae200f88cc6124e2e88c662b3f3a7ba
Raf <7909000@gmail.com>
2014-01-31 19:16:56 +0500

    ADD proper key mapping
    Working in progress... still needs to finish how this handles certain special keys

--- src/thaana.angular.js
+++ src/thaana.angular.js
@@ -1,11 +1,12 @@
 (function () {
     'use strict';
     /* global angular */
+    
 
     angular.module('thaana.angular', [])
 
     .constant('keyboardLayout', {
-         113: 1968, 119: 1927, 101: 1964, 114: 1923, 116: 1932, 121: 1940, 117: 1962, 105: 1960, 111: 1966, 112: 1941, 97: 1958, 115: 1936, 100: 1931, 102: 1930, 103: 1934, 104: 1920, 106: 1942, 107: 1926, 108: 1933, 122: 1938, 120: 215, 99: 1943, 118: 1928, 98: 1924, 110: 1922, 109: 1929, 81: 1956, 87: 1954, 69: 1965, 82: 1948, 84: 1939, 89: 1952, 85: 1963, 73: 1961, 79: 1967, 80: 247, 65: 1959, 83: 1921, 68: 1937, 70: 65010, 71: 1955, 72: 1945, 74: 1947, 75: 1946, 76: 1925, 90: 1953, 88: 1944, 67: 1949, 86: 1957, 66: 1950, 78: 1935, 77: 1951
+         32:160, 190:65106, 59:1563, 188: 1548, 113: 1968, 119: 1927, 101: 1964, 114: 1923, 116: 1932, 121: 1940, 117: 1962, 105: 1960, 111: 1966, 112: 1941, 97: 1958, 115: 1936, 100: 1931, 102: 1930, 103: 1934, 104: 1920, 106: 1942, 107: 1926, 108: 1933, 122: 1938, 120: 215, 99: 1943, 118: 1928, 98: 1924, 110: 1922, 109: 1929, 81: 1956, 87: 1954, 69: 1965, 82: 1948, 84: 1939, 89: 1952, 85: 1963, 73: 1961, 79: 1967, 80: 247, 65: 1959, 83: 1921, 68: 1937, 70: 65010, 71: 1955, 72: 1945, 74: 1947, 75: 1946, 76: 1925, 90: 1953, 88: 1944, 67: 1949, 86: 1957, 66: 1950, 78: 1935, 77: 1951
     })
 
     /**
@@ -23,6 +24,9 @@
             thaanaConfig[setKey] = setValue;
         };
 
+        var isMac = navigator.platform.toUpperCase().indexOf('MAC')!==-1;
+        var isWindows = navigator.platform.toUpperCase().indexOf('WIN')!==-1;
+        var isLinux = navigator.platform.toUpperCase().indexOf('LINUX')!==-1;
         
 
         var ngThaanaService = ['$rootScope', 'keyboardLayout', function($rootScope, keyboardLayout) {
@@ -33,25 +37,18 @@
 
             var translateToThaanaCharacter = function (keyCode) {
                 
+                
+
                 if(thaanaConfig.keyboardLayout[keyCode] !== undefined) {
-                    var char = translateKeyToCharacter(thaanaConfig.keyboardLayout[keyCode]);
-                    return char;
+                    var charecter = translateKeyToCharacter(thaanaConfig.keyboardLayout[keyCode]);
+                    return charecter;
                 }  else {
                     return translateKeyToCharacter(keyCode);
                 }
-                
             };
 
-
-            /**
-             * Islolated Scope
-             * @type {ngScope}
-             */
-            var $scope = $rootScope.$new(true);
+            //set language
             thaanaConfig.keyboardLayout = keyboardLayout;
-
-            $scope.originalContent = '';
-            $scope.translatedThaanaContent = '';
 
             return {
                 config: function (key, setValue) {
@@ -62,51 +59,29 @@
                     }
                 },
 
-
-
-                originalContent : function () {
-                    return $scope.originalContent;
-                },
-
-                setOriginalContent : function (content) {
-                    $scope.originalContent = content;
-                },
-
-                getContent : function () {
-                    return $scope.translatedContent;
-                },
-
-                setTranslatedContent : function (content) {
-                    $scope.translatedContent = content;
-                },
-
-                onKeypress : function (charCode) {
-             
-                    var originHistory = angular.copy(this.originalContent());
-                    var origin;
-
-                    if(originHistory !== undefined) {
-                        origin = originHistory + translateKeyToCharacter(charCode);
+                translate: function(keyCode, isShift) {
+                    if(isShift) {
+                        //check if the code range is a number key and if number key, we skip translation
+                        return translateToThaanaCharacter(keyCode);
                     } else {
-                        origin = translateKeyToCharacter(charCode);
+                        //normalize key 
+                       var latin = translateKeyToCharacter(keyCode);
+                       var normalChar = latin;
+                       
+                       if(keyCode > 64) {
+                            normalChar = latin.toLowerCase();
+                       } else {
+                            console.log('below alphabet '+keyCode);
+                       }
+                      
+                       keyCode = normalChar.charCodeAt(0);
+
+                       console.log('converting latin '+latin+ ' to '+normalChar+' with code '+keyCode);
+                       
+                       return translateToThaanaCharacter(keyCode);
                     }
+
                     
-                    this.setOriginalContent(origin);
-
-                        
-
-                    //translate
-                    var transContentHistory = angular.copy(this.getContent());
-                    var transContent;
-
-                    if(transContentHistory !== undefined) {
-                        transContent = transContentHistory + translateToThaanaCharacter(charCode);
-                    } else {
-                        transContent = translateToThaanaCharacter(charCode);
-                    }
-                    
-                    this.setTranslatedContent(transContent);
-                   
                 }
             };
 
@@ -118,49 +93,106 @@
         };
     })
 
-
-
     .directive('ngThaana', ['$log', 'ngThaana', function ($log, ngThaana) {
         
-        var LinkFn = function(scope, element, attr, ngModel) {
+        var getKeyCode = function(keyEvent) {
+            var e = keyEvent || window.event;
+            if(keyEvent)
+            {
+                if(typeof e.which == "number") {
+                     var charCode = e.which;
+                     return charCode;
+                }
+            }
+            
+           
+            return (window.event ? keyEvent.keyCode : keyEvent.which);
+        };
+
+        var LinkFn = function(scope, element, attr) {
+
+            scope.thaanakeyboardActive = false;
 
             scope.$watch(function() {
                 return ngThaana.config('activated');
             }, function (state) {
                 if(state) {
-                     element.addClass('thaana');
-                     element.attr('dir', 'rtl');
+                    element.addClass('thaana');
+                    element.attr('dir', 'rtl');
+                    scope.thaanakeyboardActive = true;
                 } else {
-                     element.removeClass('thaana');
-                      element.removeAttr('dir');
+                    element.removeClass('thaana');
+                    element.removeAttr('dir');
+                    scope.thaanakeyboardActive = false;
                 }
             });
 
 
-            element.bind('keypress', function(evt) {
-                evt.preventDefault();
-                ngThaana.onKeypress(evt.which);
-                var content = ngThaana.getContent();
-                //update ngModel
-                
+            scope.$watch('thaanakeyboardActive', function(activated) {
 
-                scope.$apply(function() {
-                    if(ngModel !== undefined) {
-                        ngModel.$setViewValue(content);
-                    }
-                });
+                if(activated) {
+                    console.log('activated');
+                    scope.keypressThaana = false;
+                    scope.shiftKeyDown = false;
+
+                    element.bind('keyup', function(evt) {
+                        var currentkeyCode = getKeyCode(evt);
+                        //Deactivate ShiftKey Press
+                        if(currentkeyCode === 16) {
+                            scope.shiftKeyDown = false;
+                        }
+                    });
+
+                    element.bind('keydown', function(evt) {
+                
+                        var currentkeyCode = getKeyCode(evt);
+
+                        //activate shiftKey Press
+                        if(currentkeyCode === 16) {
+                            scope.shiftKeyDown = true;
+                        }
+
+                        //escape controlKey, Alt Key, CommandKey(Meta) Key on MAC
+                        //we want to escape translating any keys when any of the special keys are down
+                        if(evt.ctrlKey === true || evt.altKey === true || evt.metaKey === true || (currentkeyCode < 32 && currentkeyCode !== 8))
+                        {
+                            console.log('special key');
+                            return true;
+                        } else {
+                            // handle backspace
+                            if(currentkeyCode == 8) {
+                                $(this).sendkeys('{backspace}');
+                            } else {
+
+                                // when shift is down and pressed on number keys, we skip translation
+                                if(scope.shiftKeyDown === true && currentkeyCode < 65) {
+                                    return true;
+                                } else {
+
+                                    evt.preventDefault();
+                                    var Character = ngThaana.translate(currentkeyCode, scope.shiftKeyDown); //String.fromCharCode(currentkeyCode);
+                                   
+                                    $(this).sendkeys(Character);
+                                }
+
+                                
+                            }
+                        }   
+
+                        
+                    });
+                } else {
+                    scope.keypressThaana = false;
+                    scope.shiftKeyDown = false;
+
+                    element.unbind();
+                }
             });
 
-           
         };
-
         return {
             restrict: 'A',
-            require: '?ngModel',
             link: LinkFn
         };
     }]);
-
-
-
 })();
